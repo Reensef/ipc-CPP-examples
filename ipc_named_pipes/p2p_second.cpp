ee1de41258b91291ebf5b4e2350596c3b2dd2ecb
kirillreensef <ki.b2310111@gmail.com>
2024-03-05 00:21:01 +0700

    Add named pipes example/change shared memory example

---
+++ ipc_named_pipes/p2p_second.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <error.h>
+#include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+#include <thread>
+#include <chrono>
+
+#define PERMISSION_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+struct ThreadParams
+{
+    char *namePipeOne;
+    char *namePipeTwo;
+};
+
+void *read(void *threadParams)
+{
+    ThreadParams *params = (ThreadParams *)(threadParams);
+
+    while (true)
+    {
+        char data[1000];
+
+        int id = open(params->namePipeOne, O_RDONLY);
+
+        if (id == -1)
+        {
+            std::cout << "Error open pipe" << std::endl;
+        }
+
+        read(id, data, 1000);
+        close(id);
+
+        std::cout << "Recived message: " << data << std::endl;
+    }
+}
+
+void *write(void *threadParams)
+{
+    ThreadParams *params = (ThreadParams *)(threadParams);
+
+    while (true)
+    {
+        char inputString[1000];
+        std::cin >> inputString;
+
+        int id = open(params->namePipeTwo, O_WRONLY);
+
+        if (id == -1)
+        {
+            std::cout << "Error open pipe" << std::endl;
+        }
+
+        write(id, inputString, 1000);
+
+        close(id);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    char namePipeOne[] = "/tmp/fifo.1";
+    char namePipeTwo[] = "/tmp/fifo.2";
+
+    mkfifo(namePipeOne, PERMISSION_MODE);
+    mkfifo(namePipeTwo, PERMISSION_MODE);
+
+    std::cout << "You are ready to receive and send messages:" << std::endl;
+    std::cout << "Messages must be no more than 1000 characters long" << std::endl;
+
+    ThreadParams *threadParam = new ThreadParams{namePipeOne, namePipeTwo};
+    pthread_t writeThread, readThread;
+
+    if (pthread_create(&readThread, NULL, read, (void *)threadParam) != 0)
+    {
+        std::cerr << "Error creating readThread. Try reinstalling the application." << std::endl;
+        return 1;
+    }
+
+    if (pthread_create(&writeThread, NULL, write, (void *)threadParam) != 0)
+    {
+        std::cerr << "Error creating thread writeThread. Try reinstalling the application." << std::endl;
+        return 1;
+    }
+
+    pthread_join(readThread, NULL);
+    pthread_join(writeThread, NULL);
+
+    // semctl(semId, 0, IPC_RMID, NULL);
+    // unlink(argv[1]);
+
+    return 0;
+}
---
+++ ipc_shared_memory/message_driven.cpp
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <error.h>
+#include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+#include <csignal>
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+#include <thread>
+#include <chrono>
+
+#define PERMISSION_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+void lock(int semid)
+{
+    struct sembuf operation = {0, -1, 0};
+    if (semop(semid, &operation, 1) == -1)
+    {
+        perror("semop");
+        exit(1);
+    }
+}
+
+void unlock(int semid)
+{
+    struct sembuf operation = {0, 1, 0};
+    if (semop(semid, &operation, 1) == -1)
+    {
+        perror("semop");
+        exit(1);
+    }
+}
+
+struct ThreadParams
+{
+    const int semId;
+    const int segReadId;
+};
+
+void *read(void *threadParams)
+{
+    ThreadParams *params = (ThreadParams *)(threadParams);
+    char *ptr;
+
+    while (true)
+    {
+        lock(params->semId);
+
+        ptr = (char *)shmat(params->segReadId, NULL, 0);
+
+        // Сheck for yours
+        char pidString[7];
+        int arrayId = 0;
+        while (*ptr != ':')
+        {
+            pidString[arrayId++] = *ptr;
+            ptr++;
+        }
+        ptr++;
+
+        pid_t pid = getpid();
+
+        if (pid != std::stoi(pidString))
+        {
+            std::cout << "From " << pidString << ": " << ptr << std::endl;
+        }
+
+        unlock(params->semId);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void *write(void *threadParams)
+{
+    ThreadParams *params = (ThreadParams *)(threadParams);
+
+    char *ptr;
+    pid_t pid = getpid();
+
+    while (true)
+    {
+
+        char inputString[1000];
+        std::cin >> inputString;
+
+        char newString[1007];
+        sprintf(newString, "%d:%s", pid, inputString);
+
+        ptr = (char *)shmat(params->segReadId, NULL, 0);
+
+        std::strcpy(ptr, newString);
+
+        unlock(params->semId);
+        lock(params->semId);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // Create file for semaphore
+    char sharedFileName[] = "/tmp/com.sharedMemory.file";
+    std::ofstream file(sharedFileName);
+    if (file.is_open())
+    {
+        file.close();
+    }
+    else
+    {
+        std::cerr << "Error creation semaphore file. Try reinstalling the application." << std::endl;
+        return 1;
+    }
+
+    int oflag = PERMISSION_MODE | IPC_CREAT;
+
+    key_t semKey = ftok(sharedFileName, 'S');
+    int semId = semget(semKey, 1, 0666 | IPC_CREAT);
+
+    if (semId == -1)
+    {
+        std::cerr << "Semaphore creation error" << std::endl;
+        return 1;
+    }
+
+    key_t segReadKey = ftok(sharedFileName, 'R');
+    int segReadId = shmget(segReadKey, 1007, oflag);
+
+    if (segReadId == -1)
+    {
+        std::cout << "Error accessing shared memory segment" << std::endl;
+        return 1;
+    }
+
+    std::cout << "You are ready to receive and send messages" << std::endl;
+    std::cout << "Messages must be no more than 1000 characters long" << std::endl;
+
+    ThreadParams *threadParam = new ThreadParams{semId, segReadId};
+    pthread_t writeThread, readThread;
+
+    if (pthread_create(&readThread, NULL, read, (void *)threadParam) != 0)
+    {
+        std::cerr << "Error creating thread 1" << std::endl;
+        return 1;
+    }
+
+    if (pthread_create(&writeThread, NULL, write, (void *)threadParam) != 0)
+    {
+        std::cerr << "Error creating thread 2" << std::endl;
+        return 1;
+    }
+
+    pthread_join(readThread, NULL);
+    pthread_join(writeThread, NULL);
+
+    return 0;
+}
